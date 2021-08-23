6114139a8b9a8aa48b26f644ba8ce6c2f28ca87f
Drew Hanberry <drew.hanberry1@gmail.com>
2021-08-23 11:27:58 -0700

    fix nav padding

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="container" style={{paddingTop: 'env(safe-area-inset-top)'}}>
-        <nav className="navbar fixed-top bg-white border-bottom justify-content-center" style={{paddingTop: 'env(safe-area-inset-top)'}}>
+        <nav className="navbar fixed-top bg-white border-bottom justify-content-center" style={{paddingTop: 'calc(8px + env(safe-area-inset-top))'}}>
             <h5 className="mb-0">Coffee calculator</h5>
         </nav>
         <div style={{marginTop: '57px'}}>
